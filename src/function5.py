e00a792672269902cd54ed6815701767310c85ac
imregemes <imre.2000.cime@gmail.com>
2022-10-18 19:47:35 +0200

    ...

--- src/function5.py
+++ src/function5.py
@@ -1,3 +1,9 @@
 def function5(input_string: str, input_int: int) -> str:
-    """Write your code here"""
+    lista = []
+    for i in int(len(input_string)):
+        list.append(lista[5*i-1])
+    lovak = "".join(lista)
+
+    return(lovak)
+    
     pass
