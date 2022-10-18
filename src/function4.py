f291c2f2ae82de309988506ff494f42722987771
imregemes <imre.2000.cime@gmail.com>
2022-10-18 19:35:39 +0200

    .

--- src/function4.py
+++ src/function4.py
@@ -1,3 +1,6 @@
 def function4(input_int1: int, input_int2: int) -> int:
-    """Write your code here"""
-    pass
+    for x in range (0, input_int1):
+        if (input_int1 - x) % x == 0 and (input_int2 - x) % x == 0:
+            return(input_int1 - x)
+
+            
