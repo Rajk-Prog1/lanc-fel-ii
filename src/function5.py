ca7be0b93e1f4c5c48894539be8741d746dcb476
imregemes <imre.2000.cime@gmail.com>
2022-10-18 19:52:26 +0200

    ...

--- src/function5.py
+++ src/function5.py
@@ -1,6 +1,6 @@
 def function5(input_string: str, input_int: int) -> str:
     lista = []
-    for i in int(len(input_string)):
+    for i in range(len(input_string)):
         list.append(lista[input_int*i - 1])
     lovak = "".join(lista)
 
